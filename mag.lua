914c6c1c9c47a7c1853a1dd5c787c5024e1ec333
n3veruary <45745887+n3veruary@users.noreply.github.com>
2021-03-05 16:32:02 -0500

    Add files via upload

--- mag.lua
+++ mag.lua
@@ -8,58 +8,56 @@
     * Npc: Magtheridon
 --]]
 local Mag = {}
--- on spawn
-local function Mag.EnterCombat(event,creature,target)
+
+local function Mag.EnterCombat(event,creature,target) -- emote on combat entry, declare events for casts.
 local nearest = creature:GetNearestPlayer(200,0,1)
 creature:Emote(-1544015)
 creature:Emote(-154406)
 creature:RegisterEvent(Mag.Berserk,1200000,1)
 creature:RegisterEvent(Mag.Conflag, {10000, 15000}, 0)
 creature:RegisterEvent(Mag.Quake, 40000, 0)
-creature:RegisterEvent(Mag.Nova, 55000)
 creature:RegisterEvent(Mag.Cleave, 15000)
 creature:ClearThreatList()
 Unit:Attack(nearest)
 end
--- on player kill
-local function Mag.KillsPlayer(event, creature, victim)
+
+local function Mag.KillsPlayer(event, creature, victim) -- send message when player dies, select next victim
 creature:Emote(-1544010)
 creature:SelectVictim()
 end
---on leaving combat
-local function Mag.OnDrop(event, creature)
+
+local function Mag.OnDrop(event, creature) -- wipe events when leaving combat
 creature:RemoveEvents()
 end
---on death
-local function Mag.OnDeath(event, creature, killer)
+
+local function Mag.OnDeath(event, creature, killer) -- say message. wipe events on death.
+creature:SendChatMessageToPlayer(4,0,"The Legion will consume you all!")
 creature:RemoveEvents()
 end
---berserk
-local function Mag.Berserk(event, delay, pCall, creature)
+
+local function Mag.Berserk(event, delay, pCall, creature) -- berserk. cast on timer.
 creature:CastSpell(self,27680,true)
 end
---conflag
-local function Mag.Conflag(event, delay, pCall, creature)
+
+local function Mag.Conflag(event, delay, pCall, creature) -- conflag. cast on timer. select random target on ground.
         local x,y,z = creature:GetRelativePoint(math.random()*9, math.random()*math.pi*2)
         local conflag = creature:CastSpellAoF( x, y, z, 30757)
 end
---quake
-local function Mag.Quake(event, delay, pCall, creature)
+
+local function Mag.Quake(event, delay, pCall, creature) -- quake, cast on timer.
 creature:CastSpell(30657)
 end
---nova
-local function Mag.Nova(event, delay, pCall, creature)
-    if !creature:HasAura(30168) then
-	creature:CastSpell(30616)
-	else
+
+local function Mag.Cage(event, creature, caster, spellid) -- cage handler. when hit by shadow grasp, apply dummy aura and stop movement. need to count to 5.
+    if creature:HasAura(30410) then
 	creature:SetRooted()
 	creature:AttackStop()
 	creature:MoveIdle()
 	creature:CastSpell(30205)
 end
 end
---cleave
-local function Mag.Cleave(event, delay, pCall, creature)
+
+local function Mag.Cleave(event, delay, pCall, creature) -- cleave
 creature:SelectVictim()
 creature:CastSpell(30619)
 end
@@ -68,3 +66,4 @@ RegisterCreatureEvent(17257, 1, Mag.EnterCombat)
 RegisterCreatureEvent(17257, 2, Mag.OnDrop)
 RegisterCreatureEvent(17257, 3, Mag.KillsPlayer)
 RegisterCreatureEvent(17257, 4, Mag.OnDeath)
+RegisterCreatureEvent(17257, 14, Mag.Cage)
